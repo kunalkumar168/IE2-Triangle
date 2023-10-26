ed3132ba0aa087f22b226b5f73c75f2e7e99f675
Kunal Kumar <kunal.kumar.eee14@itbhu.ac.in>
2023-10-25 23:34:21 -0400

    Adding statement coverage test suite.

---
+++ test_suit/test_statementCoverage.py
@@ -0,0 +1,21 @@
+import unittest
+from isTriangle import Triangle
+
+class TestTriangleClassification(unittest.TestCase):
+
+    def test_invalid_triangle(self):
+        self.assertEqual(Triangle.classify(0, 0, 0), Triangle.Type.INVALID)
+        self.assertEqual(Triangle.classify(1, 2, 5), Triangle.Type.INVALID)
+        self.assertEqual(Triangle.classify(-5, 5, 5), Triangle.Type.INVALID)
+        self.assertEqual(Triangle.classify(2, 3, 5), Triangle.Type.INVALID)
+
+    def test_scalene_triangle(self):
+        self.assertEqual(Triangle.classify(3, 4, 5), Triangle.Type.SCALENE)
+
+    def test_equilateral_triangle(self):
+        self.assertEqual(Triangle.classify(5, 5, 5), Triangle.Type.EQUILATERAL)
+
+    def test_isosceles_triangle(self):
+        self.assertEqual(Triangle.classify(5, 5, 8), Triangle.Type.ISOSCELES)
+        self.assertEqual(Triangle.classify(5, 8, 5), Triangle.Type.ISOSCELES)
+        self.assertEqual(Triangle.classify(8, 5, 5), Triangle.Type.ISOSCELES)
