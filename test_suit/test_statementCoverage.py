9a3d42ad9097a359710d717c8b7911128dc96e2f
Kunal Kumar <kunal.kumar.eee14@itbhu.ac.in>
2023-10-26 11:59:51 -0400

    Updating statement test suite with some test case.

--- test_suit/test_statementCoverage.py
+++ test_suit/test_statementCoverage.py
@@ -8,6 +8,7 @@ class TestTriangleClassification(unittest.TestCase):
         self.assertEqual(Triangle.classify(1, 2, 5), Triangle.Type.INVALID)
         self.assertEqual(Triangle.classify(-5, 5, 5), Triangle.Type.INVALID)
         self.assertEqual(Triangle.classify(2, 3, 5), Triangle.Type.INVALID)
+        self.assertEqual(Triangle.classify(5, 5, 10), Triangle.Type.INVALID)
 
     def test_scalene_triangle(self):
         self.assertEqual(Triangle.classify(3, 4, 5), Triangle.Type.SCALENE)
@@ -19,3 +20,6 @@ class TestTriangleClassification(unittest.TestCase):
         self.assertEqual(Triangle.classify(5, 5, 8), Triangle.Type.ISOSCELES)
         self.assertEqual(Triangle.classify(5, 8, 5), Triangle.Type.ISOSCELES)
         self.assertEqual(Triangle.classify(8, 5, 5), Triangle.Type.ISOSCELES)
+
+if __name__ == '__main__':
+    unittest.main()
